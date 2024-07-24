6d9bc85845291338912d164de0c5c5a58b2767b7
lati111 <75079614+lati111@users.noreply.github.com>
2024-07-24 09:35:04 +0200

    Added null filter

---
+++ src/Filters/NullFilter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Lati111\LaravelDataproviders\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @class Filter by a standard string
+ */
+class NullFilter extends AbstractFilter
+{
+    /** {@inheritdoc} */
+    protected string $type = 'bool';
+
+    /** {@inheritdoc} */
+    protected function getOperators(): array {
+        return [
+            ['operator' => '=', 'text' => 'is'],
+            ['operator' => '!=', 'text' => 'is not'],
+        ];
+    }
+    /** {@inheritdoc} */
+    public function handle(Builder $builder, string $operator, string|null $value = null): Builder {
+        return parent::handle($builder, $operator, null);
+    }
+
+
+    /** {@inheritdoc} */
+    protected function getOptions(): array {
+        return [null];
+    }
+}
